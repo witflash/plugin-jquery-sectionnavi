326f72147d40d00e4a02e64627f3c6dbb996cff7
Andrew Mambyk <mambyk.andrew@gmail.com>
2018-03-19 11:48:33 +0200

    add init function

--- jquery.sectionnavi.js
+++ jquery.sectionnavi.js
@@ -1,22 +1,29 @@
-(function($){
+( function($) {
 
-    $.fn.sectionnavi = function(options) {
-        // Зададим список свойств и укажем для них значения по умолчанию.
-        // Если при вызове метода будут указаны пользовательские
-        // варианты некоторых из них, то они автоматически перепишут
-        // соответствующие значения по умолчанию
-        var options = $.extend({
-            param1: 'param1Value', //параметр1
-            param2: 'param2Value' //параметр2
-        }, options);
+    var methods = {
+        init : function(options) {
+            var options = $.extend({
+                section : 'section', //параметр1
+                buttonUp : 'buttonUp', //параметр2
+            }, options);
 
-		var make = function(){
-			// реализация работы метода с отдельным элементом страницы
-			console.log(this);
-		};
+            return this.each(methods.navigation);
+        },
 
-		return this.each(make);
-		// в итоге, метод pluginName вернет текущий объект jQuery обратно
+        navigation : function() {
+            var $this = $(this);
+            return $this;
+        }
+    }
+
+    $.fn.sectionnavi = function(method) {
+        if (methods[method]) {
+            return methods[method].apply( this, Array.prototype.slice.call(arguments, 1));
+        } else if ( typeof method === 'object' || !method ) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error(`${method} is'n exist in jQuery.sectionnavi`);
+        };
 	};
 
-})(jQuery);
+} )(jQuery);
